3c3413026f96253465b941fe8af07deefd10ce14
dmi3coder <dmi3coder@gmail.com>
2016-01-21 13:48:37 +0200

    Login check process, bundle injected, FIX of bundle

--- src/github/dmi3coder/syncodoro/Main.java
+++ src/github/dmi3coder/syncodoro/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
     private Arc progressArc;
     private Scene rootScene;
     public static Preferences preferences = Preferences.userNodeForPackage(Main.class);
-    public static ResourceBundle bundle = ResourceBundle.getBundle("strings");
+    public static ResourceBundle bundle = ResourceBundle.getBundle("github.dmi3coder.syncodoro.strings");
     static final Client mKinveyClient = new Client.Builder(
             bundle.getString("APPID"),
             bundle.getString("APPSECRET")).build();
@@ -32,20 +32,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        rootScene = new Scene(root,640, 420);
-        progressTimer = (Label) rootScene.lookup("#progressTimer");
-        progressTimer.setText(preferences.get("minutes","25")+":" +preferences.get("seconds","00"));
-        progressArc = (Arc)rootScene.lookup("#progressCircle");
-        primaryStage.setTitle("Syncodoro");
-        primaryStage.setScene(rootScene);
-        primaryStage.show();
-
         if(preferences.getBoolean("isFirstLogin",true)){
             //preferences.putBoolean("isFirstLogin",false);
             showLoginForm();
             defineStandardValues();
             new Login(mKinveyClient).start(new Stage());
         }
+        rootScene = new Scene(root,640, 420);
+        progressTimer = (Label) rootScene.lookup("#progressTimer");
+        progressTimer.setText(preferences.getInt("session_minutes",25)+":" +preferences.getInt("session_seconds",0));
+        progressArc = (Arc)rootScene.lookup("#progressCircle");
+        primaryStage.setTitle("Syncodoro");
+        primaryStage.setScene(rootScene);
+        primaryStage.show();
+
+
     }
 
 
